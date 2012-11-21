4b966a547c2b9803b41b8d958a52f63b0e9e16d4
uchisawa tsumugi <utti.gimu@gmsil.com>
2012-11-21 19:54:20 +0900

    リポジトリの作成

---
+++ carrier.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        carrier
+// @namespace   https://gs.cuc.ac.jp/cuc/campus
+// @description   キャリアカルテの書き換え
+// @include https://gs.cuc.ac.jp/cuc/campus?view=view.menu&func=function.shushoku.careerchart.search.input
+// ==/UserScript==
+
+alert("ようこそ！");
